de0c8f1b255e444ae50a30a6d7fdf80184c8a4ab
Samet Koyuncu <sametkoyuncu@live.com>
2021-11-23 19:34:01 +0300

    Remove box component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import caseTown from 'case-town'
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
 
-// search bar
+// mui components
+import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
 import InputBase from '@mui/material/InputBase'
 import Divider from '@mui/material/Divider'
@@ -11,37 +10,16 @@ import IconButton from '@mui/material/IconButton'
 import SearchIcon from '@mui/icons-material/Search'
 //styles
 import './styles.css'
-/* Arkaplanlar
-back 2: https://i.hizliresim.com/blk6siu.png
-back 3: https://i.hizliresim.com/db2yim8.png
-back 4: https://i.hizliresim.com/30rgrsj.png
-back 6: https://i.hizliresim.com/syp78b9.png
-back 7: https://i.hizliresim.com/9uy3umu.png
-BACK 8: https://i.hizliresim.com/675wqbb.png
-BACK 9: https://i.hizliresim.com/hfuzcgx.png
-mobile clicked: https://i.hizliresim.com/qd9eye4.png
-mobile 2: https://i.hizliresim.com/cnwfc2s.png
-back 10 mobile: https://i.hizliresim.com/l0j76wd.png
-*/
 
 function SearchArea({ setSearchQuery }) {
   const [isClicked, setIsClicked] = useState(false)
   const [searchText, setSearchText] = useState('')
 
-  const afterFirstClickStyles = {
-    height: '175px',
-    transition: '1.5s',
-  }
-
-  const beforeFirstClickStyles = {
-    height: '100vh',
-  }
-
   // searchText events start
   const handleChange = (e) => {
     setSearchText(e.target.value)
   }
-
+  // gelen metindeki istenmeyen karakterleri temizle
   const clearSearchText = () => {
     let clearedText = caseTown.justClear(searchText)
     let clearedTextAndAddedPlus = caseTown.searchQuery(searchText)
@@ -51,9 +29,9 @@ function SearchArea({ setSearchQuery }) {
     }
     return values
   }
-
-  const handleClick = () => {
-    if (searchText === '') {
+  // verileri güncelleyecek method
+  const setStates = () => {
+    if (searchText.trim() === '') {
       return
     }
     const { text, query } = clearSearchText()
@@ -64,52 +42,38 @@ function SearchArea({ setSearchQuery }) {
     setIsClicked(true)
   }
 
-  const handleEnter = (e) => {
-    if (searchText === '') {
-      return
-    }
+  const handleClick = () => setStates()
 
-    if (e.keyCode === 13) {
-      const { text, query } = clearSearchText()
-      // arama çubuğundaki metin
-      setSearchText(text)
-      // api'ye gidecek metin (boşluk yerine artı olacak şekilde)
-      setSearchQuery(query)
-      setIsClicked(true)
-    }
-  }
+  const handleEnter = (e) => (e.keyCode === 13 ? setStates() : null)
+
   return (
-    <Box
-      sx={isClicked ? afterFirstClickStyles : beforeFirstClickStyles}
-      className="search-box"
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      sx={isClicked ? { height: 175 } : { height: '100vh' }}
+      className="search-grid"
     >
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        sx={isClicked ? afterFirstClickStyles : beforeFirstClickStyles}
-      >
-        <Paper className="search-paper" elevation={0}>
-          <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="Search Books"
-            inputProps={{ 'aria-label': 'search books' }}
-            value={searchText}
-            onChange={handleChange}
-            onKeyDown={(e) => handleEnter(e)}
-          />
-          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-          <IconButton
-            sx={{ p: '10px' }}
-            onClick={handleClick}
-            aria-label="search"
-          >
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-      </Grid>
-    </Box>
+      <Paper className="search-paper" elevation={0}>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Books"
+          inputProps={{ 'aria-label': 'search books' }}
+          value={searchText}
+          onChange={handleChange}
+          onKeyDown={(e) => handleEnter(e)}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton
+          sx={{ p: '10px' }}
+          onClick={handleClick}
+          aria-label="search"
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </Grid>
   )
 }
 
