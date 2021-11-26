9108b91d69bc5296d08f28b09b280595a4822d28
Nurselin Çavdar <58823845+nrslncvdr@users.noreply.github.com>
2021-11-26 22:04:00 +0300

    modal ortaya alindi ve stil değisti.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,28 +1,55 @@
-import React from 'react'
+import React from "react";
 import "./style.css";
 
-function Modal({ showModal/* , openModal */ ,detailModal}) {
-  return showModal && detailModal ? (
-      <section className="modal container">
-
-        <div className="modal__container" id="modal-container">
-          <div className="modal__content">
-            <div className="modal__close close-modal" title="Close">
-              <i className="bx bx-x"/*  onClick={() => { openModal(false) }} */></i>
-            </div>
-            <div className="img_container">
-              <img src={detailModal.volumeInfo.imageLinks.thumbnail} alt="Book Picture" className="modal__img" />
-            </div>
-            <div className="text_container">
-              <p className="modal__description">{detailModal.volumeInfo.description.substring(0,180)} ...</p>
-              <p className="modal__author">Author - {detailModal.volumeInfo.authors[0]} </p>
-            </div>
-            <button className="modal__button" /* onClick={() => { openModal(false) }} */> Close </button>
-            <button className="modal__button"> Preview </button>
+function Modal({ modal, selectedBook, setModal }) {
+  return modal && selectedBook ? (
+    <section className="modal container ">
+      <div className="modal__container" id="modal-container">
+        <div className="modal__content centered ">
+          <div className="modal__close close-modal" title="Close">
+            <i
+              className="bx bx-x"
+              onClick={() => {
+                setModal(false);
+              }}
+            ></i>
           </div>
+          <div className="img_container">
+            <img
+              src={selectedBook.volumeInfo.imageLinks.thumbnail }
+              alt="Book Picture"
+              className="modal__img"
+            />
+          </div>
+          <div className="text_container">
+            <p className="modal__description">
+              {selectedBook.volumeInfo.description == null ? "No details!": (selectedBook.volumeInfo.description.substring(0, 180)+"...")}
+            </p>
+            <p className="book__detail">
+             {selectedBook.volumeInfo.publishedDate}
+            </p>
+            <p className="book__detail">
+             {selectedBook.volumeInfo.pageCount == null ? "No pages!" : (selectedBook.volumeInfo.pageCount)+ " pages"} 
+            </p>
+            <p className="book__detail">
+              Author - {selectedBook.volumeInfo.authors == null ? "No authors!" : selectedBook.volumeInfo.authors.map(a => a 
+              )} </p>
+          </div>
+          <button
+            className="modal__button"
+            onClick={() => {
+              setModal(false);
+            }}
+          >
+            Close
+          </button>
+          <button className="modal__button">
+            <a target="_blank" href={selectedBook.volumeInfo.infoLink}>Preview</a>
+          </button>
         </div>
-      </section>
-    ) : null
+      </div>
+    </section>
+  ) : null;
 }
 
 export default Modal;
