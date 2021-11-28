35d5a8e5fac8730754d30546d04176fae506ac0b
Nurselin Çavdar <58823845+nrslncvdr@users.noreply.github.com>
2021-11-28 12:56:52 +0300

    modal dark mod stillendirilmesi yapıldı.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,12 +4,14 @@ import img from "../images/img.png";
 import { useTheme } from "../../context/ThemeContext";
 
 function Modal({ modal, selectedBook, setModal }) {
+  const { theme } = useTheme()
+
   return modal && selectedBook ? (
 
     <section className="modal container ">
-      <div className="modal__container" id="modal-container">
-        <div className="modal__content centered ">
-          <div className="modal__close close-modal" title="Close">
+      <div className="modal__container" id="modal-container" >
+        <div className="modal__content centered " style={theme === 'dark' ? { backgroundColor: '#424242', color:'#9e9e9e'} : null}>
+          <div className="modal__close close-modal" title="Close" >
             <i
               className="bx bx-x"
               onClick={() => {
@@ -27,30 +29,31 @@ function Modal({ modal, selectedBook, setModal }) {
               className="modal__img"
             />
           </div>
-          <div className="text_container">
-            <p className="modal__description">
+          <div className="text_container" style={theme === 'dark' ? {color:'#9e9e9e'} : null}>
+            <p className="modal__description" >
               {selectedBook.volumeInfo.description == null ? "No details!": (selectedBook.volumeInfo.description.substring(0, 180)+"...")}
             </p>
-            <p className="book__detail">
+            <p className="book__detail" style={theme === 'dark' ? {color:'#9e9e9e'} : null}>
              {selectedBook.volumeInfo.publishedDate}
             </p>
-            <p className="book__detail">
+            <p className="book__detail" style={theme === 'dark' ? {color:'#9e9e9e'} : null}>
              {selectedBook.volumeInfo.pageCount == null ? "No pages!" : (selectedBook.volumeInfo.pageCount)+ " pages"} 
             </p>
-            <p className="book__detail" >
+            <p className="book__detail" style={theme === 'dark' ? {color:'#9e9e9e'} : null}>
             Authors  <br/>{selectedBook.volumeInfo.authors == null ? "No authors!" : selectedBook.volumeInfo.authors.map(a=> <> {a} <br/></>)} </p>
-          </div>
-          <button
+            <button
             className="modal__button"
             onClick={() => {
               setModal(false);
-            }}
-          >
-            Close
+            }} 
+            style={theme === 'dark' ? {color:'#424242'} : null}
+          >Close
           </button>
-          <button className="modal__button">
+          <button className="modal__button" style={theme === 'dark' ? {color:'#424242'} : null}>
             <a target="_blank" href={selectedBook.volumeInfo.infoLink}>Preview</a>
           </button>
+          </div>
+          
         </div>
       </div>
     </section>
