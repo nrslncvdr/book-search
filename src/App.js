7dcfc51944d750c2174dffb3eb717395a5e68c1f
Samet Koyuncu <sametkoyuncu@live.com>
2021-11-29 19:43:09 +0300

    Skeleton test

--- src/App.js
+++ src/App.js
@@ -1,60 +1,56 @@
-import { useState, useEffect } from "react";
-import SearchArea from "./components/Search";
-import Cards from "./components/Cards/Cards";
-import LoadMoreBtn from "./components/LoadMoreBtn";
-import axios from "axios";
-import Modal from "./components/Modal";
-import { useTheme } from "./context/ThemeContext";
+import { useState, useEffect } from 'react'
+import SearchArea from './components/Search'
+import Cards from './components/Cards/Cards'
+import LoadMoreBtn from './components/LoadMoreBtn'
+import axios from 'axios'
+import Modal from './components/Modal'
+import { useTheme } from './context/ThemeContext'
+import CardSkeleton from './components/Skeleton/CardSkeleton'
 
 function App() {
-  const { theme, setTheme } = useTheme();
-  const [books, setBooks] = useState([]);
-  const [selectedBook, setSelectedBook] = useState({});
-  const [searchQuery, setSearchQuery] = useState("");
-  const [page, setPage] = useState(0);
-  const [modal, setModal] = useState(false);
+  const { theme, setTheme } = useTheme()
+  const [books, setBooks] = useState([])
+  const [selectedBook, setSelectedBook] = useState({})
+  const [searchQuery, setSearchQuery] = useState('')
+  const [page, setPage] = useState(0)
+  const [modal, setModal] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const getBooks = async (varsaEskiVeriler) => {
-    if (searchQuery === "") {
-      return;
+    if (searchQuery === '') {
+      return
     }
     try {
-      const index = page * 12;
+      setIsLoading(true)
+      const index = page * 12
       const { data } = await axios(
         `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&startIndex=${index}&maxResults=12`
-      );
-      setBooks([...varsaEskiVeriler, ...data.items]);
+      )
+      setBooks([...varsaEskiVeriler, ...data.items])
+      setIsLoading(false)
     } catch (e) {
-      console.log("error: ", e);
+      console.log('error: ', e)
     }
-  };
+  }
 
   useEffect(() => {
-    setBooks([]);
-    getBooks([]);
-  }, [searchQuery]);
+    setBooks([])
+    getBooks([])
+  }, [searchQuery])
 
   useEffect(() => {
-    getBooks(books);
-  }, [page]);
+    getBooks(books)
+  }, [page])
 
   const showModal = () => {
-    setModal(true);
-  };
+    setModal(true)
+  }
 
-  const bgColor = theme === "light" ? "#fff" : "#505050";
-  console.log(theme);
+  const bgColor = theme === 'light' ? '#fff' : '#505050'
+  console.log(theme)
   return (
     <div style={{ backgroundColor: bgColor }}>
       <SearchArea setSearchQuery={setSearchQuery} />
-      {books.length === 0 && theme === "dark" && searchQuery.length !== "" ? (
-        <div
-          style={{
-            width: "100%",
-            height: "100vh",
-            backgroundColor: "#505050",
-          }}
-        ></div>
-      ) : null}
+      {books.length < 1 && isLoading ? <CardSkeleton /> : null}
       {books.length > 1 ? (
         <>
           <Cards
@@ -63,7 +59,7 @@ function App() {
             showModal={showModal}
             isLoading={isLoading}
           />
-
+          {isLoading ? <CardSkeleton /> : null}
           <LoadMoreBtn page={page} setPage={setPage} />
           <Modal
             selectedBook={selectedBook}
@@ -74,7 +70,7 @@ function App() {
         </>
       ) : null}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
