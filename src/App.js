8e40f98d27634cf11c3d1725db27fbac6a187acb
Fr3shBrain <aylanc.onur@gmail.com>
2021-11-30 15:53:21 +0300

    Skeleton fixed

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-import { useState, useEffect } from 'react'
-import SearchArea from './components/Search'
-import Cards from './components/Cards/Cards'
-import LoadMoreBtn from './components/LoadMoreBtn'
-import axios from 'axios'
-import Modal from './components/Modal'
-import { useTheme } from './context/ThemeContext'
-import CardSkeleton from './components/Skeleton/CardSkeleton'
+import { useState, useEffect } from "react";
+import SearchArea from "./components/Search";
+import Cards from "./components/Cards/Cards";
+import LoadMoreBtn from "./components/LoadMoreBtn";
+import axios from "axios";
+import Modal from "./components/Modal";
+import { useTheme } from "./context/ThemeContext";
+import CardSkeleton from "./components/Skeleton/CardSkeleton";
 
 function App() {
-  const { theme, setTheme } = useTheme()
-  const [books, setBooks] = useState([])
-  const [selectedBook, setSelectedBook] = useState({})
-  const [searchQuery, setSearchQuery] = useState('')
-  const [page, setPage] = useState(0)
-  const [modal, setModal] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const { theme, setTheme } = useTheme();
+  const [books, setBooks] = useState([]);
+  const [selectedBook, setSelectedBook] = useState({});
+  const [searchQuery, setSearchQuery] = useState("");
+  const [page, setPage] = useState(0);
+  const [modal, setModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const getBooks = async (varsaEskiVeriler) => {
-    if (searchQuery === '') {
-      return
+    if (searchQuery === "") {
+      return;
     }
     try {
-      setIsLoading(true)
-      const index = page * 12
+      setIsLoading(true);
+      const index = page * 12;
       const { data } = await axios(
         `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&startIndex=${index}&maxResults=12`
-      )
-      setBooks([...varsaEskiVeriler, ...data.items])
-      setIsLoading(false)
+      );
+      setBooks([...varsaEskiVeriler, ...data.items]);
+      setIsLoading(false);
     } catch (e) {
-      console.log('error: ', e)
+      console.log("error: ", e);
     }
-  }
+  };
 
   useEffect(() => {
-    setBooks([])
-    getBooks([])
-  }, [searchQuery])
+    setBooks([]);
+    getBooks([]);
+  }, [searchQuery]);
 
   useEffect(() => {
-    getBooks(books)
-  }, [page])
+    getBooks(books);
+  }, [page]);
 
   const showModal = () => {
-    setModal(true)
-  }
+    setModal(true);
+  };
 
-  const bgColor = theme === 'light' ? '#fff' : '#505050'
-  console.log(theme)
+  const bgColor = theme === "light" ? "#fff" : "#505050";
+  
   return (
     <div style={{ backgroundColor: bgColor }}>
       <SearchArea setSearchQuery={setSearchQuery} />
@@ -70,7 +70,7 @@ function App() {
         </>
       ) : null}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Skeleton/CardSkeleton.js
+++ src/components/Skeleton/CardSkeleton.js
@@ -1,12 +1,61 @@
-import { Box, Skeleton } from "@mui/material";
+import { Container, Grid, Box, Skeleton } from "@mui/material";
 
 const CardSkeleton = () => {
   return (
-    <Box sx={{ width: 300 }}>
-      <Skeleton />
-      <Skeleton animation="wave" />
-      <Skeleton animation={false} />
-    </Box>
+    <>
+      <Container fixed sx={{ p: 5 }}>
+        <Grid container spacing={2.5}>
+          <Grid container spacing={2} item xs={12} sm={6} md={3}>
+            <Box>
+              <Skeleton
+                variant="rectangular"
+                width={230}
+                height={300}
+                sx={{ borderRadius: 2.5, marginBottom: 1 }}
+              />
+              <Skeleton sx={{ marginBottom: 0.5 }} animation="wave" />
+              <Skeleton animation="wave" />
+            </Box>
+          </Grid>
+          <Grid container spacing={2} item xs={12} sm={6} md={3}>
+            <Box>
+              <Skeleton
+                variant="rectangular"
+                width={230}
+                height={300}
+                sx={{ borderRadius: 2.5, marginBottom: 1 }}
+              />
+              <Skeleton sx={{ marginBottom: 0.5 }} animation="wave" />
+              <Skeleton animation="wave" />
+            </Box>
+          </Grid>
+          <Grid container spacing={2} item xs={12} sm={6} md={3}>
+            <Box>
+              <Skeleton
+                variant="rectangular"
+                width={230}
+                height={300}
+                sx={{ borderRadius: 2.5, marginBottom: 1 }}
+              />
+              <Skeleton sx={{ marginBottom: 0.5 }} animation="wave" />
+              <Skeleton animation="wave" />
+            </Box>
+          </Grid>
+          <Grid container spacing={2} item xs={12} sm={6} md={3}>
+            <Box>
+              <Skeleton
+                variant="rectangular"
+                width={230}
+                height={300}
+                sx={{ borderRadius: 2.5, marginBottom: 1 }}
+              />
+              <Skeleton sx={{ marginBottom: 0.5 }} animation="wave" />
+              <Skeleton animation="wave" />
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
