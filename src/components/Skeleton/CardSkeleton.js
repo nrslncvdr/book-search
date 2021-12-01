0298aa18c8f0ff4a023d9fd87ecc4b97ee983f0d
Fr3shBrain <aylanc.onur@gmail.com>
2021-12-01 20:41:15 +0300

    skeleton responsive fixed

--- src/components/Skeleton/CardSkeleton.js
+++ src/components/Skeleton/CardSkeleton.js
@@ -10,40 +10,56 @@ const CardSkeleton = () => {
               variant="rectangular"
               width={230}
               height={300}
-              sx={{ borderRadius: 2.5, marginBottom: 1 }}
+              sx={{ borderRadius: 2.5, marginBottom: 1, mx: "auto" }}
             />
-            <Skeleton width={230} sx={{ marginBottom: 0.5 }} animation="wave" />
-            <Skeleton width={230} animation="wave" />
+            <Skeleton
+              width={230}
+              sx={{ marginBottom: 0.5, mx: "auto" }}
+              animation="wave"
+            />
+            <Skeleton width={230} animation="wave" sx={{ mx: "auto" }} />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
             <Skeleton
               variant="rectangular"
               width={230}
               height={300}
-              sx={{ borderRadius: 2.5, marginBottom: 1 }}
+              sx={{ borderRadius: 2.5, marginBottom: 1, mx: "auto" }}
             />
-            <Skeleton width={230} sx={{ marginBottom: 0.5 }} animation="wave" />
-            <Skeleton width={230} animation="wave" />
+            <Skeleton
+              width={230}
+              sx={{ marginBottom: 0.5, mx: "auto" }}
+              animation="wave"
+            />
+            <Skeleton width={230} animation="wave" sx={{ mx: "auto" }} />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
             <Skeleton
               variant="rectangular"
               width={230}
               height={300}
-              sx={{ borderRadius: 2.5, marginBottom: 1 }}
+              sx={{ borderRadius: 2.5, marginBottom: 1, mx: "auto" }}
             />
-            <Skeleton width={230} sx={{ marginBottom: 0.5 }} animation="wave" />
-            <Skeleton width={230} animation="wave" />
+            <Skeleton
+              width={230}
+              sx={{ marginBottom: 0.5, mx: "auto" }}
+              animation="wave"
+            />
+            <Skeleton width={230} animation="wave" sx={{ mx: "auto" }} />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
             <Skeleton
               variant="rectangular"
               width={230}
               height={300}
-              sx={{ borderRadius: 2.5, marginBottom: 1 }}
+              sx={{ borderRadius: 2.5, marginBottom: 1, mx: "auto" }}
             />
-            <Skeleton width={230} sx={{ marginBottom: 0.5 }} animation="wave" />
-            <Skeleton width={230} animation="wave" />
+            <Skeleton
+              width={230}
+              sx={{ marginBottom: 0.5, mx: "auto" }}
+              animation="wave"
+            />
+            <Skeleton width={230} animation="wave" sx={{ mx: "auto" }} />
           </Grid>
         </Grid>
       </Container>
