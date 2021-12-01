6a225ff9f7427cf2acf620ae8c9bce579da44de8
Fr3shBrain <aylanc.onur@gmail.com>
2021-12-01 18:15:26 +0300

    cards responsive fixed

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -6,7 +6,7 @@ const Cards = ({ books, setSelectedBook, showModal, isLoading }) => {
     <Container fixed sx={{ p: 2.5 }}>
       <Grid container spacing={2}>
         {books?.map((book) => (
-          <Grid key={book.id} item xs={12} sm={6} md={3}>
+          <Grid key={book.id} item xs={12} sm={6} md={4} lg={3}>
             <BookCard
               book={book}
               setSelectedBook={setSelectedBook}
